aec4f82ab5d7005a65a5a0f9a4376958f5de9f20
MNK Coders <jaume.llopis1@gmail.com>
2019-11-11 17:11:26 +0100

    Asociación Deportiva - Frecuencia cardíaca

--- AssociacioEsportiva/public_html/assets/fcm.js
+++ AssociacioEsportiva/public_html/assets/fcm.js
@@ -3,9 +3,86 @@
  * @returns {CalculadoraFCM}
  */
 function CalculadoraFCM(){
+    /**
+     * @type CalculadoraFCM
+     */
+    var _self = this;
+    /**
+     * 
+     * @param {Number} fcm
+     * @returns {String}
+     */
+    this.clasificacion = function( fcm ){
+        
+        console.log( fcm );
+        
+        //var pct = fcm / 100;
+
+        switch( true ){
+            case ( fcm >= 60 && fcm < 70 ):
+                return 'Zona de recuperación (60%-70%)';
+            case ( fcm >= 70 && fcm < 80 ):
+                return 'Zona aer&oacute;bica (70%-80%)';
+            case ( fcm >= 80 && fcm < 90 ):
+                return 'Zona anaer&oacute;bica (80%-90%)';
+            case ( fcm >= 90 && fcm < 100 ):
+                return 'L&iacute;nea Roja (90%-100%)';
+        }
+        
+        return 'valores incorrectos';
+    };
+    /**
+     * 
+     * @param {type} edad
+     * @param {type} sexo
+     * @returns {Number}
+     */
+    this.calcular = function( edad , sexo ){
+        
+        switch( sexo ){
+            case CalculadoraFCM.Sexo.HOMBRE:
+                return ( ( 209 - parseInt(edad) ) * 0.7 ).toFixed(2);
+            case CalculadoraFCM.Sexo.MUJER:
+                return ( ( 214 - parseInt(edad) ) * 0.8 ).toFixed(2);
+        }
+        
+        return 0.0;
+    };
+    /**
+     * @param {Number} fcm
+     * @returns {CalculadoraFCM}
+     */
+    this.display = function( fcm ){
+        
+        var output = document.getElementById('fcm_output');
+        
+        if( output !== null ){
+            
+            output.innerHTML = '<h3>FCM: <span>'
+                    + fcm + '</span></h3>'
+                    + '<p>' + this.clasificacion( fcm ) + '</p>';
+            
+        }
+        
+        return this;
+    };
     
-    
+    /**
+     * @returns {CalculadoraFCM}
+     */
     this.bind = function(){
+        
+        var edad = document.getElementById('fcm_edad');
+        var sexo = document.getElementById('fcm_sexo');
+        
+        document.getElementById('fcm_button').addEventListener( 'click' , function(e){
+            
+            e.preventDefault();
+            
+            _self.display(_self.calcular(edad.value,sexo.value));
+            
+            return true;
+        });
         
         console.log('Form FCM inicializado');
         
@@ -14,3 +91,12 @@ function CalculadoraFCM(){
     
     return this.bind();
 }
+/**
+ * @type CalculadoraFCM.Sexo
+ */
+CalculadoraFCM.Sexo = {
+    HOMBRE: 'h',
+    MUJER: 'm',
+};
+
+
--- AssociacioEsportiva/public_html/assets/imc.js
+++ AssociacioEsportiva/public_html/assets/imc.js
@@ -5,6 +5,10 @@
  * @returns {CalculadoraIMC}
  */
 function CalculadoraIMC(){
+    /**
+     * @type CalculadoraIMC
+     */
+    var _self = this;
     
     var _inputs = {
         'height': null,
@@ -14,28 +18,109 @@ function CalculadoraIMC(){
     };
     
     var _settings = {
-        
+
     };
-    
+    /**
+     * @param {Number} imc
+     * @returns {Number}
+     */
+    this.clasificacion = function( imc ){
+        
+        switch( true ){
+            case ( imc < 16 ): //<16.00: Infrapeso (delgadez severa)
+                return CalculadoraIMC.Clasificacion.DELGADEZ_SEVERA;
+            case (imc >= 16 && imc < 17): //16.00 – 16.99: Infrapeso (delgadez moderada)
+                return CalculadoraIMC.Clasificacion.DELGADEZ_MODERADA;
+            case (imc >= 17 && imc < 18.50 ): //17.00 - 18.49: Infrapeso (delgadez aceptable)
+                return CalculadoraIMC.Clasificacion.DELGADEZ_ACEPTABLE;
+            case (imc >= 18.50 && imc < 25 ): //18.50 - 24.99: Peso normal
+                return CalculadoraIMC.Clasificacion.NORMAL;
+            case (imc >= 25 && imc < 30 ): //25.00 - 29.99: Sobrepeso
+                return CalculadoraIMC.Clasificacion.SOBREPESO;
+            case ( imc >= 30 && imc < 35 ): //30.00 - 34.99: Obeso (Tipo I)
+                return CalculadoraIMC.Clasificacion.OBESO_TIPO_1;
+            case ( imc >= 35 && imc <= 40 ): //35.00 - 40.00: Obeso (Tipo II)
+                return CalculadoraIMC.Clasificacion.OBESO_TIPO_2;
+            case ( imc > 40 ): //>40.00: Obeso (Tipo III)
+                return CalculadoraIMC.Clasificacion.OBESO_TIPO_3;
+        }
+    };
+    /**
+     * @param {Number} imc
+     * @returns {String}
+     */
+    this.mostrarClasificacion = function( imc ){
+        //console.log(this.clasificacion( imc ));
+        switch( this.clasificacion( imc ) ){
+            case CalculadoraIMC.Clasificacion.DELGADEZ_SEVERA:
+                return 'Infrapeso (delgadez severa)';
+            case CalculadoraIMC.Clasificacion.DELGADEZ_MODERADA:
+                return 'Infrapeso (delgadez moderada)';
+            case CalculadoraIMC.Clasificacion.DELGADEZ_ACEPTABLE:
+                return 'Infrapeso (delgadez aceptable)';
+            case CalculadoraIMC.Clasificacion.NORMAL:
+                return 'Peso normal';
+            case CalculadoraIMC.Clasificacion.SOBREPESO:
+                return 'Sobrepeso';
+            case CalculadoraIMC.Clasificacion.OBESO_TIPO_1:
+                return 'Obeso (Tipo I)';
+            case CalculadoraIMC.Clasificacion.OBESO_TIPO_2:
+                return 'Obeso (Tipo II)';
+            case CalculadoraIMC.Clasificacion.OBESO_TIPO_3:
+                return 'Obeso (Tipo III)';
+        }
+        return 'clasificacion invalida';
+    };
+    /**
+     * @param {Number} altura
+     * @param {Number} peso
+     * @returns {Number}
+     */
+    this.calcular = function( peso , altura ){
+       
+       var a = parseFloat( altura );
+
+       var p = parseFloat( peso );
+       
+       return a > 0 ? ( p / a ).toFixed(2) : 0.0;
+    };
+    /**
+     * 
+     * @param {Number} imc
+     * @returns {CalculadoraIMC}
+     */
+    this.display = function( imc ){
+        
+        console.log(output);
+
+        var output = document.getElementById('imc_output');
+        
+        output.innerHTML = '<h3>IMC: <span>'
+                + imc + '</span></h3>'
+                + '<p>' + this.mostrarClasificacion( imc ) + '</p>';
+        
+        return this;
+    };
     /**
      * 
      * @returns {CalculadoraIMC}
      */
     this.bind = function(){
         
-        _inputs.height = document.getElementById('imc_height');
+        var height = document.getElementById('imc_height');
         
-        _inputs.width = document.getElementById('imc_width');
+        var weight = document.getElementById('imc_weight');
 
-        _inputs.button = document.getElementById('imc_button');
+        //_inputs.output = document.getElementById('imc_output');
         
-        _inputs.output = document.getElementById('imc_output');
-        
-        _inputs.button.addEventListener( 'click' , function(e){
+        document.getElementById('imc_button').addEventListener( 'click' , function(e){
             
             e.preventDefault();
             
-            console.log('clicked!!');
+            //console.log('clicked!!');
+            _self.display(_self.calcular(
+                    weight.value,
+                    height.value));
             
             return true;
         });
@@ -45,6 +130,31 @@ function CalculadoraIMC(){
         return this;
     };
     
-    
     return this.bind();
 }
+
+/**
+<16.00: Infrapeso (delgadez severa)
+■ 16.00 – 16.99: Infrapeso (delgadez moderada)
+■ 17.00 - 18.49: Infrapeso (delgadez aceptable)
+■ 18.50 - 24.99: Peso normal
+■ 25.00 - 29.99: Sobrepeso
+■ 30.00 - 34.99: Obeso (Tipo I)
+■ 35.00 - 40.00: Obeso (Tipo II)
+■ >40.00: Obeso (Tipo III)
+ * @type type
+ */
+CalculadoraIMC.Clasificacion = {
+    DELGADEZ_SEVERA: 1,
+    DELGADEZ_MODERADA: 2,
+    DELGADEZ_ACEPTABLE: 3,
+    NORMAL: 4,
+    SOBREPESO: 5,
+    OBESO_TIPO_1: 6,
+    OBESO_TIPO_2: 7,
+    OBESO_TIPO_3: 8
+};
+
+
+
+

116199209b764c58e2c374a437b42b685cf7abd4
mnkcoders <jaume.llopis1@gmail.com>
2019-12-05 19:58:54 +0100

    Unidad 3

--- unidad3/public_html/modal.js
+++ unidad3/public_html/modal.js
@@ -1,33 +1,58 @@
-
-
-function Modal(){
-    
+/**
+ * @param {String} title 
+ * @returns {Modal}
+ */
+function Modal( title ){
     
     var _setup = {
-        'container': null,
+        'title': title || 'New Modal',
+        'window': null,
         'options': []
     };
-    
-    this.form = function(){
+    /**
+     * 
+     * @returns {Modal}
+     */
+    this.show = function(){
         
+        _setup.window = window.open( '' , "modal", "width=600, height=400, scrollbars=no");
+        //console.log(_setup.window);
+        if( _setup.window === null ){
+            console.log('Activate PopUps!!');
+            return this;
+        }
         
+        _setup.window.document.head.innerHTML = _setup.window.parent.document;
         
-        _setup.container = document.createElement('div');
-        
-        
-        
+        _setup.window.document.title = _setup.title;
+               
+        _setup.options.forEach( function( option ){
+            
+            if( typeof option.callback === 'function' ){
+                
+                var button = document.createElement('button');
+
+                button.innerHTML = option.title;
+
+                button.addEventListener('click', option.callback );
+                
+                var line_option = document.createElement('p');
+                
+                line_option.appendChild(button);
+
+                _setup.window.document.body.appendChild( line_option );
+            } 
+        });
         
         return this;
     };
-    
-    
     /**
      * 
      * @param {String} option
      * @param {Function} callback
      * @returns {Modal}
      */
-    this.bindOption = function( option , callback ){
+    this.bind = function( option , callback ){
         
         _setup.options.push({
             'title': option,
@@ -38,14 +63,9 @@ function Modal(){
     };
     
     
-    /**
-     * 
-     * @returns {Modal}
-     */
-    this.init = function(){
-        
-        
-        
-        return this;
-    };
+    return this;
 }
+/**
+ * @returns {Modal}
+ */
+Modal.create = function( title ){ return new Modal( title ); };

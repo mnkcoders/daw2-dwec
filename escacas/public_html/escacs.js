4da83f6d8e70399cbb3bb3a73e791b3b9240c710
unknown <jaume.llopis1@gmail.com>
2020-03-18 10:17:23 +0100

    Chess

--- escacas/public_html/escacs.js
+++ escacas/public_html/escacs.js
@@ -1,19 +1,174 @@
+function Juez(){
+    
+    this.evaluar = function(  ){
+        
+        return true;
+    };
+    
+    return this;
+}
+
 /**
  * @param {String} id
  * @returns {Escacs}
  */
-function Escacs( id){
+function Escacs( id ){
     
     var _escacs = {
-        'this': this,
-        'id': typeof id === 'string' && id.length ? id : 'escacs'
+        'id': typeof id === 'string' && id.length ? id : 'escacs',
+        'juego':{
+            'turno':0,
+            'jugador': 'Negro',
+            'mueve': false
+        },
+        'estado': Escacs.Estado.Iniciando
     };
+    
+    var _juez = new Juez();
+    
     /**
      * @returns {String}
      */
     this.id = () => '#' + _escacs.id;
     
+    this.turno = function( id ){
+        
+        switch( true ){
+            case _escacs.juego.mueve === false:
+                var ficha = this.validarCasilla( id );
+                if( this.validarFicha( ficha ) ){
+                    //capturar la pieza que selecciona el jugador para mover
+                    _escacs.juego.mueve = id;
+                    this.notifica( _escacs.juego.jugador
+                            + ' selecciona ' + ficha + ' - '
+                            + this.nombrarCasilla(_escacs.juego.mueve) );
+                }
+                else{
+                    this.notifica( _escacs.juego.jugador + ': selecciona una de tus piezas');
+                }
+                break;
+            case typeof _escacs.juego.mueve === 'number':
+                //evaluar el movimiento
+                if( _juez.evaluar( _escacs.juego.mueve , id ) ){
+                    var destino = this.validarCasilla( id );
+                    this.notifica( _escacs.juego.jugador
+                            + ' avanza sobre ' + destino + ' - '
+                            + this.nombrarCasilla( id ) );
+                    this.moverPieza(_escacs.juego.mueve , id );
+                    //resetea y reinicia
+                    this.siguienteTurno();
+                }
+                else{
+                    this.notifica( _escacs.juego.jugador + ': no se admite ese movimiento');
+                }
+                break;
+        }
+    };
+    /**
+     * @returns {Escacs}
+     */
+    this.siguienteTurno = function(){
+        
+        _escacs.juego.mueve = false;
+        
+        _escacs.juego.jugador = _escacs.juego.jugador === 'Blanco' ?
+                'Negro' :
+                'Blanco';
+        
+        if( _escacs.estado < Escacs.Estado.Jugando ){
+
+            _escacs.estado = Escacs.Estado.Jugando;
+            this.notifica('Empieza ' + _escacs.juego.jugador );
+            //console.log('--> Juega el ' + _escacs.juego.jugador );
+        }
+        
+        return this;
+    };
+    /**
+     * @param {Number} desde
+     * @param {Number} hasta
+     * @returns {Escacs}
+     */
+    this.moverPieza = function( desde , hasta ){
+        
+        var seleccion = jQuery( '.icon[data-cell='+desde+']' );
+        var destino = jQuery( '.icon[data-cell='+hasta+']' );
+        
+        var pieza = jQuery( seleccion ).html();
+        jQuery( destino ).html( pieza );
+        jQuery( destino ).attr('data-id',jQuery(seleccion).attr('data-id'));
+        jQuery( seleccion ).html('');
+        jQuery( seleccion ).attr('data-id','');
+        
+        return this;
+    };
+    /**
+     * @param {Number} id
+     * @returns {String}
+     */
+    this.nombrarCasilla = function( id ){
+        
+        if( typeof id !== 'number' ){
+            id = parseInt( id );
+        }
+        
+        var letras = ['A','B','C','D','E','F','G','H'];
+
+        var fila = parseInt( id / 8 );
+        
+        var columna = (id % 8) + 1;
+        
+        return letras[ fila ] + ':' + columna;
+    };
+    /**
+     * @param {Number} id
+     * @returns {String}
+     */
+    this.validarCasilla = function( id ){
+        
+        var casilla = jQuery('.icon[data-cell=' + id + ']');
+        
+        return jQuery( casilla ).attr('data-id');
+    };
+    /**
+     * @param {String} ficha
+     * @returns {Boolean}
+     */
+    this.validarFicha = function( ficha ){
+        switch( _escacs.juego.jugador ){
+            case 'Negro':
+                for( var pieza in Escacs.Jugadores.Negro ){
+                    if( ficha === Escacs.Jugadores.Negro[pieza] ){
+                        return true;
+                    }
+                }
+                break;
+            case 'Blanco':
+                for( var pieza in Escacs.Jugadores.Blanco ){
+                    if( ficha === Escacs.Jugadores.Blanco[pieza] ){
+                        return true;
+                    }
+                }
+                break;
+        }
+        return false;
+    };
+    /**
+     * @param {String} mensaje
+     * @returns {Escacs}
+     */
+    this.notifica = function( mensaje ){
+
+        jQuery('.notifier').html( mensaje );
+        
+        return this;
+    };
+    /**
+     * @returns {Escacs}
+     */
     this.crearTablero = () =>{
+        
+        var _self = this;
         
         var table = jQuery('<table class="tablero">');
  
@@ -22,24 +177,125 @@ function Escacs( id){
             for( var j = 0 ; j < 8 ; j++ ){
                 var td = jQuery('<td>')
                 var celda = jQuery('<span class="icon">');
-                jQuery(celda).attr('data-id', (i*8) + j );
+                jQuery(celda).attr('data-cell', (i*8) + j );
+                
+                jQuery(celda).on('click',function(e){
+                    e.preventDefault();
+                    
+                    if( _escacs.estado === Escacs.Estado.Jugando ){
+                        
+                        _self.turno( parseInt( jQuery(this).attr('data-cell') ) );
+                        
+                        return true;
+                    }
+                    
+                    return false;
+                });
+                
                 jQuery(td).append(celda);
                 jQuery(tr).append(td);
             }
             jQuery(table).append(tr);
         }
         
+        var notifier = jQuery('<h4 class="notifier">');
+        
+        jQuery( this.id() ).append( notifier );
         jQuery( this.id() ).append( table );
+        
+        return this;
+    };
+    /**
+     * @returns {Escacs}
+     */
+    this.crearLayout = function(){
+        
+        jQuery('.icon').attr('data-id','');
+        jQuery('.icon').html('');
+        
+        jQuery('.icon[data-cell=0]').html(Escacs.Pieza.Torre);
+        jQuery('.icon[data-cell=0]').attr('data-id',Escacs.Jugadores.Blanco.Torre);
+        jQuery('.icon[data-cell=1]').html(Escacs.Pieza.Caballo);
+        jQuery('.icon[data-cell=1]').attr('data-id',Escacs.Jugadores.Blanco.Caballo);
+        jQuery('.icon[data-cell=2]').html(Escacs.Pieza.Alfil);
+        jQuery('.icon[data-cell=2]').attr('data-id',Escacs.Jugadores.Blanco.Alfil);
+        jQuery('.icon[data-cell=3]').html(Escacs.Pieza.Rey);
+        jQuery('.icon[data-cell=3]').attr('data-id',Escacs.Jugadores.Blanco.Rey);
+        jQuery('.icon[data-cell=4]').html(Escacs.Pieza.Reina);
+        jQuery('.icon[data-cell=4]').attr('data-id',Escacs.Jugadores.Blanco.Reina);
+        jQuery('.icon[data-cell=5]').html(Escacs.Pieza.Alfil);
+        jQuery('.icon[data-cell=5]').attr('data-id',Escacs.Jugadores.Blanco.Alfil);
+        jQuery('.icon[data-cell=6]').html(Escacs.Pieza.Caballo);
+        jQuery('.icon[data-cell=6]').attr('data-id',Escacs.Jugadores.Blanco.Caballo);
+        jQuery('.icon[data-cell=7]').html(Escacs.Pieza.Torre);
+        jQuery('.icon[data-cell=7]').attr('data-id',Escacs.Jugadores.Blanco.Torre);
+
+        jQuery('.icon[data-cell=8]').html(Escacs.Pieza.Peon);
+        jQuery('.icon[data-cell=8]').attr('data-id',Escacs.Jugadores.Blanco.Peon);
+        jQuery('.icon[data-cell=9]').html(Escacs.Pieza.Peon);
+        jQuery('.icon[data-cell=9]').attr('data-id',Escacs.Jugadores.Blanco.Peon);
+        jQuery('.icon[data-cell=10]').html(Escacs.Pieza.Peon);
+        jQuery('.icon[data-cell=10]').attr('data-id',Escacs.Jugadores.Blanco.Peon);
+        jQuery('.icon[data-cell=11]').html(Escacs.Pieza.Peon);
+        jQuery('.icon[data-cell=11]').attr('data-id',Escacs.Jugadores.Blanco.Peon);
+        jQuery('.icon[data-cell=12]').html(Escacs.Pieza.Peon);
+        jQuery('.icon[data-cell=12]').attr('data-id',Escacs.Jugadores.Blanco.Peon);
+        jQuery('.icon[data-cell=13]').html(Escacs.Pieza.Peon);
+        jQuery('.icon[data-cell=13]').attr('data-id',Escacs.Jugadores.Blanco.Peon);
+        jQuery('.icon[data-cell=14]').html(Escacs.Pieza.Peon);
+        jQuery('.icon[data-cell=14]').attr('data-id',Escacs.Jugadores.Blanco.Peon);
+        jQuery('.icon[data-cell=15]').html(Escacs.Pieza.Peon);
+        jQuery('.icon[data-cell=15]').attr('data-id',Escacs.Jugadores.Blanco.Peon);
+        
+        
+        jQuery('.icon[data-cell=56]').html(Escacs.Pieza.Torre);
+        jQuery('.icon[data-cell=56]').attr('data-id',Escacs.Jugadores.Negro.Torre);
+        jQuery('.icon[data-cell=57]').html(Escacs.Pieza.Caballo);
+        jQuery('.icon[data-cell=57]').attr('data-id',Escacs.Jugadores.Negro.Caballo);
+        jQuery('.icon[data-cell=58]').html(Escacs.Pieza.Alfil);
+        jQuery('.icon[data-cell=58]').attr('data-id',Escacs.Jugadores.Negro.Alfil);
+        jQuery('.icon[data-cell=60]').html(Escacs.Pieza.Rey);
+        jQuery('.icon[data-cell=60]').attr('data-id',Escacs.Jugadores.Negro.Rey);
+        jQuery('.icon[data-cell=59]').html(Escacs.Pieza.Reina);
+        jQuery('.icon[data-cell=59]').attr('data-id',Escacs.Jugadores.Negro.Reina);
+        jQuery('.icon[data-cell=61]').html(Escacs.Pieza.Alfil);
+        jQuery('.icon[data-cell=61]').attr('data-id',Escacs.Jugadores.Negro.Alfil);
+        jQuery('.icon[data-cell=62]').html(Escacs.Pieza.Caballo);
+        jQuery('.icon[data-cell=62]').attr('data-id',Escacs.Jugadores.Negro.Caballo);
+        jQuery('.icon[data-cell=63]').html(Escacs.Pieza.Torre);
+        jQuery('.icon[data-cell=63]').attr('data-id',Escacs.Jugadores.Negro.Torre);
+
+        jQuery('.icon[data-cell=48]').html(Escacs.Pieza.Peon);
+        jQuery('.icon[data-cell=48]').attr('data-id',Escacs.Jugadores.Negro.Peon);
+        jQuery('.icon[data-cell=49]').html(Escacs.Pieza.Peon);
+        jQuery('.icon[data-cell=49]').attr('data-id',Escacs.Jugadores.Negro.Peon);
+        jQuery('.icon[data-cell=50]').html(Escacs.Pieza.Peon);
+        jQuery('.icon[data-cell=50]').attr('data-id',Escacs.Jugadores.Negro.Peon);
+        jQuery('.icon[data-cell=51]').html(Escacs.Pieza.Peon);
+        jQuery('.icon[data-cell=51]').attr('data-id',Escacs.Jugadores.Negro.Peon);
+        jQuery('.icon[data-cell=52]').html(Escacs.Pieza.Peon);
+        jQuery('.icon[data-cell=52]').attr('data-id',Escacs.Jugadores.Negro.Peon);
+        jQuery('.icon[data-cell=53]').html(Escacs.Pieza.Peon);
+        jQuery('.icon[data-cell=53]').attr('data-id',Escacs.Jugadores.Negro.Peon);
+        jQuery('.icon[data-cell=54]').html(Escacs.Pieza.Peon);
+        jQuery('.icon[data-cell=54]').attr('data-id',Escacs.Jugadores.Negro.Peon);
+        jQuery('.icon[data-cell=55]').html(Escacs.Pieza.Peon);
+        jQuery('.icon[data-cell=55]').attr('data-id',Escacs.Jugadores.Negro.Peon);
+        
+        
+        
+        return this;
     };
     /**
      * @returns {Escacs}
      */
     this.iniciar = function(){
+        
+        var _self = this;
 
         jQuery( document ).ready( function( e ){
             //inicializar
-            _escacs.this.crearTablero();
-
+            _self.crearTablero().crearLayout().siguienteTurno();
         } );
         return this;
     };
@@ -47,6 +303,41 @@ function Escacs( id){
     
     return this.iniciar();
 }
+
+Escacs.Pieza = {
+    'Rey': '&#9818;',
+    'Reina': '&#9819;',
+    'Torre': '&#9820;',
+    'Alfil': '&#9821;',
+    'Caballo': '&#9822;',
+    'Peon': '&#9823;'
+};
+
+Escacs.Estado = {
+    'Iniciando':0,
+    'Jugando':1,
+    'Finalizado':2
+};
+
+Escacs.Jugadores = {
+    'Negro': {
+        'Torre': 'torre-negra',
+        'Caballo': 'caballo-negro',
+        'Alfil': 'alfil-negro',
+        'Peon': 'peon-negro',
+        'Rey': 'rey-negro',
+        'Reina': 'reina-negra'
+    },
+    'Blanco': {
+        'Torre': 'torre-blanca',
+        'Caballo': 'caballo-blanco',
+        'Alfil': 'alfil-blanco',
+        'Peon': 'peon-blanco',
+        'Rey': 'rey-blanco',
+        'Reina': 'reina-blanca'
+    }
+};
+
 /**
  * @type Escacs
  */
--- exercicis-ajax-promises/scripts/script.js
+++ exercicis-ajax-promises/scripts/script.js
@@ -5,7 +5,8 @@
 
     var _meteo = {
         'key': '906a992f434bb3aca14ffb6ff41010fa',
-        'api': 'https://api.openweathermap.org/data/2.5/weather',
+        //'api': 'https://api.openweathermap.org/data/2.5/weather',
+        'api': 'https://api.openweathermap.org/data/2.5/forecast',
         //'city_source': 'https://pkgstore.datahub.io/core/country-list/data_json/data/8c458f2d15d9f2119654b29ede6e45b8/data_json.json',
         'city': 'Roma',
         'range': '5dies'
@@ -18,33 +19,120 @@
     this.refresh = function () {
 
         var _self = this;
+        
+        var daily_url = this.queryDailyForecast( _meteo.city , 15 );
+        var hour_url = this.queryHourlyForecast( _meteo.city );
 
-        jQuery.ajax(this.queryURL(_meteo.city , _meteo.range ), {
+        _self.clear();
+
+        jQuery.ajax( daily_url, {
             'dataType': 'html'
         }).fail(function (error) {
+            console.log( daily_url );
             console.log('Error: ' + JSON.stringify(error));
         }).done(function (data) {
-            console.log('Response: ' + typeof data);
-            _self.capture(data);
+            console.log('Response: ' + typeof data + ' - ' + daily_url );
+
+            var forecast = JSON.parse(data);
+
+            if(  forecast.list && Array.isArray( forecast.list ) ){
+            
+                //console.log(forecast.list);
+                
+                forecast.list.forEach( function( item ){
+                    
+                    _self.templateDaily( item );
+
+                }); 
+            }
+            //_self.capture( JSON.parse(  data ) );
+        }).always(function () {
+            console.log('Call completed');
+        });
+
+        jQuery.ajax( hour_url, {
+            'dataType': 'html'
+        }).fail(function (error) {
+            console.log( hour_url);
+            console.log('Error: ' + JSON.stringify(error));
+        }).done(function (data) {
+
+            console.log('Response: ' + typeof data + ' - ' + hour_url);
+            var forecast = JSON.parse(data);
+
+            if(  forecast.list && Array.isArray( forecast.list ) ){
+            
+                _self.listHourly( _self.groupDates( forecast.list ) );
+            }
         }).always(function () {
             console.log('Call completed');
         });
 
         return this;
     };
+    this.clear = function(){
+        jQuery('#prediccio5dies').empty();
+        jQuery('#prediccio16dies').empty();
+        return this;
+    };
     /**
      * @param {String|JSON|HTML} response
      * @returns {Meteo}
      */
-    this.capture = function (response) {
-
-        if (response.length) {
-            document.getElementById('output').innerHTML = response;
-        } else {
-            document.getElementById('output').innerHTML = '<p>Error</p>'
+    this.capture = function (response , target ) {
+        
+        console.log( response );
+        
+        if( target = '5dies' ){
+             
+             
+        }
+        else if( target = '16dies' ){
+            
         }
 
         return this;
+    };
+    /**
+     * @param {Object} input
+     * @returns {Array}
+     */
+    this.groupDates = function( input ){
+        
+        var content = [];
+
+        input.forEach(function (item) {
+
+            var date = this.parseDate(item.dt);
+
+            if (content.hasOwnProperty(date)) {
+                content[date].push(item);
+            } else {
+                content[date] = [item];
+            }
+        });
+
+        return content;
+    };
+    /**
+     * @param {String} city
+     * @returns {String}
+     */
+    this.queryHourlyForecast = function( city ){
+        
+        //http://api.openweathermap.org/data/2.5/forecast?q=London&appid=906a992f434bb3aca14ffb6ff41010fa
+        return _meteo.api + '?q=' + city + '&appid=' + _meteo.key;
+    };
+    /**
+     * @param {String} city
+     * @returns {String}
+     */
+    this.queryDailyForecast = function( city , days ){
+        if( !typeof days === 'number' ){
+            days = 16;
+        }
+        //api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt={cnt}&appid={your api key}
+        return _meteo.api + '/daily?q=' + city + '&cnt=' + days + '&appid=' + _meteo.key;
     };
     /**
      * @param {String} city 
@@ -53,7 +141,7 @@
      */
     this.queryURL = function (city,range) {
 
-        
+        //return _meteo.api + '?q=' + city + '&appid=' + _meteo.key;
 
         return _meteo.api + '?q=' + city + '&mode=html&APPID=' + _meteo.key;
     };
@@ -103,20 +191,43 @@
 
         return this;
     };
+    this.parseTime = function( ts ){
+        
+        var date = new Date( ts * 1000 );
+        
+        return date.getUTCHours() + 'h';
+    };
+    this.parseDate = function( ts ){
+        var date = new Date( ts * 1000 );
+        var format = [
+            date.getUTCDate(),
+            date.getUTCMonth() + 1,
+            date.getUTCFullYear()
+        ];
+        return format.join('/');
+    };
     this.templateDaily = function( data ){
+        
         var item = jQuery('<li>');
         var container = jQuery('<div class="well well-sm text-center">');
         var date = jQuery('<b>');
-        var image = jQuery('<img>');
+        var image = jQuery('<img >');
         var weather = jQuery('<p>');
         var temp_max = jQuery('<p class="label label-warning"></p>');
         var temp_min = jQuery('<p class="label label-default"></p>');
         
-        jQuery( date ).html( data.date );
-        jQuery( temp_max ).html( data.temp_max );
-        jQuery( temp_min ).html( data.temp_min );
-        jQuery( image ).html( data.img );
-        jQuery( weather ).html( data.weather );
+        var input_date = this.parseDate(data.dt);
+        var input_max = data.temp.max;
+        var input_min = data.temp.min;
+        var input_weather_icon = data.weather[0].icon;
+        var input_weather_desc = data.weather[0].description;
+        
+        jQuery( date ).html( input_date );
+        jQuery( temp_max ).html( input_max );
+        jQuery( temp_min ).html( input_min );
+        jQuery( image ).attr('src','http://openweathermap.org/img/w/'+input_weather_icon+'.png');
+        jQuery( image ).attr('alt',input_weather_desc);
+        jQuery( weather ).html( input_weather_desc );
         
         jQuery( container ).append(date);
         jQuery( container ).append(image);
@@ -125,14 +236,35 @@
         jQuery( container ).append(temp_min);
         jQuery( item ).append( container );
 
-        jQuery('#16dies').append( item );
+        jQuery('#prediccio16dies').append( item );
+    };
+    this.listHourly = function( data ){
+        
+        var _self = this;
+        //console.log(data);
+        for( var date in data ){
+            //console.log( date );
+            //console.log( forecast );
+            //console.log( data[date] );
+            //console.log( typeof data[date] );
+            if( Array.isArray(data[date]) && data[date].length ){
+                var display_date = jQuery('<h6>');
+                var display_forecast = jQuery('<ul class="list-unstyled list-inline">');
+                jQuery(display_date).html( date );
+                data[date].forEach( function( forecast){
+                    jQuery( display_forecast ).append( _self.templateHourly( forecast ) );
+                });
+                jQuery('#prediccio5dies').append( display_date );
+                jQuery('#prediccio5dies').append( display_forecast );
+            }
+        }
     };
     /**
      * @param {Object} data
      * @returns {scriptL#4}
      */
     this.templateHourly = function( data ){
-        
+        console.log(data);
         var item = jQuery('<li>');
         var container = jQuery('<div class="well well-sm text-center">');
         var time = jQuery('<b>');
@@ -140,10 +272,16 @@
         var weather = jQuery('<p>');
         var temp = jQuery('<p class="badge"></p>');
         
-        jQuery( time ).html( data.time );
-        jQuery( temp ).html( data.temp );
-        jQuery( image ).html( data.img );
-        jQuery( weather ).html( data.weather );
+        var input_time = this.parseTime( data.dt );
+        var weather_icon = data.weather[0].icon;
+        var weather_desc = data.weather[0].description;
+        var input_temp = data.main.temp;
+        
+        jQuery( time ).html( input_time );
+        jQuery( temp ).html( input_temp );
+        jQuery( image ).attr('src','http://openweathermap.org/img/w/'+weather_icon+'.png');
+        jQuery( image ).attr('alt', weather_desc );
+        jQuery( weather ).html( weather_desc );
         
         jQuery( container ).append(time);
         jQuery( container ).append(image);
@@ -151,9 +289,9 @@
         jQuery( container ).append(temp);
         jQuery( item ).append( container );
 
-        jQuery('#5dies').append( item );
+        //jQuery('#prediccio5dies > ul').append( item );
         
-        return this;
+        return item;
     };
     /**
      * @returns {Meteo}
